5dd1b6cadd12b328a4e602c13c4ace5630dc1aeb
RaihanTantowi <raihantantowi9@gmail.com>
2021-12-10 20:01:10 +0700

    Penambahan file

---
+++ Latihan.py
@@ -0,0 +1,86 @@
+kontak = {'Ari': '081267888', 'Dina': '087677776 '}
+print("# Daftar Kontak")
+print("="*24)
+print("|# Nama | Nomor Telepon|")
+print("|"+"="*22+"|")
+print("|# Ari  |  081267888   |")
+print("|# Dina |  087677776   |")
+print("="*24+" \n")
+
+# Tampilkan kontaknya Ari
+print("# Tampilkan kontaknya Ari")
+print("="*24)
+print("|# Nama | Nomor Telepon|")
+print("|"+"="*22+"|")
+print("|# Ari  |  081267888   |")
+print("="*24)
+print('Kontak Ari: ', kontak['Ari']+' \n')
+
+# Tambahkan kontak baru dengan nama Riko, nomor 087654544
+print("# Tambahkan kontak baru dengan nama Riko, nomor 087654544")
+kontak['Riko'] = '087654544'
+print("="*24)
+print("|# Nama | Nomor Telepon|")
+print("|"+"="*22+"|")
+print("|# Ari  |  081267888   |")
+print("|# Dina |  087677776   |")
+print("|# Riko |  087654544   |")
+print("="*24)
+print('Kontak Riko: ', kontak['Riko'])
+print(kontak,' \n')
+
+# Ubah kontak Dina dengan nomor baru 088999776
+print("# Mengubah kontak Dina dengan nomor baru 088999776")
+kontak['Dina'] = '088999776'
+print("="*24)
+print("|# Nama | Nomor Telepon|")
+print("|"+"="*22+"|")
+print("|# Ari  |  081267888   |")
+print("|# Dina |  088999776   |")
+print("|# Riko |  087654544   |")
+print("="*24)
+print('Kontak Dina: ', kontak['Dina']+' \n')
+
+# Tampilkan semua nama
+print("# Tampilkan semua nama kontak")
+print("="*9)
+print("|# Nama |")
+print("|"+"="*7+"|")
+print("|# Ari  |")
+print("|# Dina |")
+print("|# Riko |")
+print("="*9)
+print('Menampilkan semua nama kontak: ', kontak.keys(), ' \n')
+
+# Tampilkan semua nomor
+print("# Tampilkan semua nomor kontak")
+print("="*18)
+print("|# Nomor Telepon |")
+print("|"+"="*16+"|")
+print("|#   081267888   |")
+print("|#   088999776   |")
+print("|#   087654544   |")
+print("="*18)
+print('Menampilkan semua nomor kontak: ', kontak.values(), ' \n')
+
+# Tampilkan daftar nama dan nomornya
+print('# Menampilkan daftar nama dan nomornya')
+print("="*24)
+print("|# Nama | Nomor Telepon|")
+print("|"+"="*22+"|")
+print("|# Ari  |  081267888   |")
+print("|# Dina |  088999776   |")
+print("|# Riko |  087654544   |")
+print("="*24)
+print('Menampilkan semua daftar kontak: ', kontak.items(), ' \n')
+
+# Hapus kontak Dina
+print('# Menghapus kontak Dina dari daftar kontak')
+print("="*24)
+print("|# Nama | Nomor Telepon|")
+print("|"+"="*22+"|")
+print("|# Ari  |  081267888   |")
+print("|# Riko |  087654544   |")
+print("="*24)
+del kontak['Dina'] 
+print('Menghapus kontak Dina: ', kontak)
